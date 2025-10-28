9a0ddfab39c642eddd447e912a53c211a096cd6f
ImSergioh <50914599+sergiomb04@users.noreply.github.com>
2025-10-28 22:11:34 +0100

    better initialUsers variable
    now if api is down returns empty array

--- livestate-nextjs/app/page.jsx
+++ livestate-nextjs/app/page.jsx
@@ -1,8 +1,10 @@
 import UserList from '@/components/UserList';
 
 export default async function Home() {
-  const res = await fetch('http://localhost:8080/api/users', { cache: 'no-store' });
-  const initialUsers = await res.json();
+  const initialUsers = await fetch('http://localhost:8080/api/users', { cache: 'no-store' })
+    .then(res => res.ok ? res.json() : [])
+    .catch(() => []);
+
   return (
     <div>
       <h1>Hello world! (Y me la pela)</h1>
