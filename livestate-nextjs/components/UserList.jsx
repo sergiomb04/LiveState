6223f0a5281c23c02f786f5de3f786e81f63df71
ImSergioh <50914599+sergiomb04@users.noreply.github.com>
2025-10-28 22:37:42 +0100

    add example log
    whenever users changes

--- livestate-nextjs/components/UserList.jsx
+++ livestate-nextjs/components/UserList.jsx
@@ -1,9 +1,14 @@
 "use client"
 
 import { useRealtimeState } from '@/lib/realtime';
+import { useEffect } from 'react';
 
 export default function UserList({ initialUsers }) {
   const [users] = useRealtimeState('users', initialUsers);
+
+  useEffect(() => {
+    console.log("USERS:", users)
+  }, [users])
 
   return (
     <ul>
