b54263a36d84de9d8652cab8186d3e4802ec5bc2
ImSergioh <50914599+sergiomb04@users.noreply.github.com>
2025-10-29 00:16:09 +0100

    Update UsersLiveStateHandler.java

--- livestate-server/src/main/java/me/imsergioh/livecore/handler/UsersLiveStateHandler.java
+++ livestate-server/src/main/java/me/imsergioh/livecore/handler/UsersLiveStateHandler.java
@@ -1,7 +1,5 @@
 package me.imsergioh.livecore.handler;
 
-import lombok.Getter;
-import me.imsergioh.livecore.auth.AuthService;
 import me.imsergioh.livecore.instance.User;
 import me.imsergioh.livecore.instance.handler.LiveStateHandler;
 import me.imsergioh.livecore.instance.handler.ProtectedTokenHandler;
@@ -9,7 +7,6 @@ import me.imsergioh.livecore.service.UserService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.socket.WebSocketSession;
 
 import java.util.*;
 
@@ -17,13 +14,6 @@ import java.util.*;
 @ProtectedTokenHandler
 @RestController
 public class UsersLiveStateHandler extends LiveStateHandler<List<User>> {
-
-    @Getter
-    private static UsersLiveStateHandler handler;
-
-    public UsersLiveStateHandler() {
-        handler = this;
-    }
 
     @Override
     @GetMapping("/api/users")
