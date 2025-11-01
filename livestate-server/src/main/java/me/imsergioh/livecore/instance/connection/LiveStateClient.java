7a27c19335ee1c57793eb272f88795b728389d69
ImSergioh <50914599+sergiomb04@users.noreply.github.com>
2025-11-01 02:20:30 +0100

    LiveStateClient: add authToken + user
    if has authToken can access to User connected (Obviamente)

--- livestate-server/src/main/java/me/imsergioh/livecore/instance/connection/LiveStateClient.java
+++ livestate-server/src/main/java/me/imsergioh/livecore/instance/connection/LiveStateClient.java
@@ -1,15 +1,23 @@
 package me.imsergioh.livecore.instance.connection;
 
 import lombok.Getter;
+import me.imsergioh.livecore.instance.User;
+import me.imsergioh.livecore.service.UserService;
+import me.imsergioh.livecore.util.JwtUtil;
 import org.springframework.web.socket.WebSocketSession;
+
+import java.util.Optional;
 
 @Getter
 public class LiveStateClient {
 
     private final WebSocketSession session;
+    private final String authToken;
+    private User user;
 
     public LiveStateClient(WebSocketSession session) {
         this.session = session;
+        this.authToken = getAuthToken(session);
     }
 
     public void onConnect() {
@@ -18,6 +26,24 @@ public class LiveStateClient {
 
     public void onDisconnect() {
         System.out.println("New web client disconnected -> " + session.getId());
+    }
+
+    public Optional<User> getUser() {
+        if (user != null) return Optional.of(user);
+        if (authToken == null) return Optional.empty();
+        var claims = JwtUtil.getClaims(authToken);
+        String username = claims.getSubject();
+        user = UserService.get().getUserByName(username);
+        if (user == null) return Optional.empty();
+        return Optional.of(user);
+    }
+
+    private static String getAuthToken(WebSocketSession session) {
+        String query = session.getUri() != null ? session.getUri().getQuery() : null;
+        if (query != null && query.startsWith("token=")) {
+            return query.substring(6);
+        }
+        return null;
     }
 
 }
