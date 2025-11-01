d0d7c4cd0605eb92b428820ff0ad010d931d37a9
ImSergioh <50914599+sergiomb04@users.noreply.github.com>
2025-11-01 02:19:26 +0100

    Update UserInfo.jsx

--- livestate-nextjs/components/UserInfo.jsx
+++ livestate-nextjs/components/UserInfo.jsx
@@ -3,9 +3,9 @@
 import { useRealtimeState } from '@/lib/realtime';
 
 export default function UserInfo({ authToken, username, initialUserData }) {
-  //const [userData] = useRealtimeState(`user/${username}`, initialUserData, authToken);
+  const [userData] = useRealtimeState(`user/${username}`, initialUserData, authToken);
 
   return (
-    <p>{JSON.stringify(initialUserData)}</p>
+    <p>{JSON.stringify(userData)}</p>
   );
 }
